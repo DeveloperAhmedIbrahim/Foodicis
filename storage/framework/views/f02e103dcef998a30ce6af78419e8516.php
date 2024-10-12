ead4cc0d24e31518599ac583e2ccccd86936eb88
Ahmed Ibrahim <siddiqui.ahmedibrahim@gmail.com>
2024-10-12 21:04:24 +0500

    Some cache.

---
+++ storage/framework/views/f02e103dcef998a30ce6af78419e8516.php
@@ -0,0 +1,51 @@
+<head>
+
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> YNEX - Bootstrap 5 Premium Admin & Dashboard Template </title>
+    <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+    <meta name="Author" content="Spruko Technologies Private Limited">
+	<meta name="keywords" content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
+    
+   <!-- Favicon -->
+<link rel="icon" href="<?php echo e(asset('assets/images/brand-logos/favicon.ico')); ?>" type="image/x-icon">
+
+<!-- Choices JS -->
+<script src="<?php echo e(asset('assets/libs/choices.js/public/assets/scripts/choices.min.js')); ?>"></script>
+
+<!-- Main Theme JS -->
+<script src="<?php echo e(asset('assets/js/main.js')); ?>"></script>
+
+<!-- Bootstrap CSS -->
+<link id="style" href="<?php echo e(asset('assets/libs/bootstrap/css/bootstrap.min.css')); ?>" rel="stylesheet">
+
+<!-- Style CSS -->
+<link href="<?php echo e(asset('assets/css/styles.min.css')); ?>" rel="stylesheet">
+
+<!-- Icons CSS -->
+<link href="<?php echo e(asset('assets/css/icons.css')); ?>" rel="stylesheet">
+
+<!-- Node Waves CSS -->
+<link href="<?php echo e(asset('assets/libs/node-waves/waves.min.css')); ?>" rel="stylesheet">
+
+<!-- Simplebar CSS -->
+<link href="<?php echo e(asset('assets/libs/simplebar/simplebar.min.css')); ?>" rel="stylesheet">
+
+<!-- Color Picker CSS -->
+<link rel="stylesheet" href="<?php echo e(asset('assets/libs/flatpickr/flatpickr.min.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('assets/libs/@simonwep/pickr/themes/nano.min.css')); ?>">
+
+<!-- Choices CSS -->
+<link rel="stylesheet" href="<?php echo e(asset('assets/libs/choices.js/public/assets/styles/choices.min.css')); ?>">
+
+<!-- JS Vector Maps CSS -->
+<link rel="stylesheet" href="<?php echo e(asset('assets/libs/jsvectormap/css/jsvectormap.min.css')); ?>">
+
+<!-- Swiper CSS -->
+<link rel="stylesheet" href="<?php echo e(asset('assets/libs/swiper/swiper-bundle.min.css')); ?>">
+
+
+</head>
+<?php /**PATH D:\laragon\www\Foodicis\resources\views/admin_panel/admin_dashboard/layout/header.blade.php ENDPATH**/ ?>
